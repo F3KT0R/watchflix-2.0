06efc8f08585024c37bb355cb6e85a0078a3069c
Tibor Bálint <balinttibornb@gmail.com>
2023-01-25 16:19:49 +0100

    minor styling fix

--- src/components/functional/ContentCard.js
+++ src/components/functional/ContentCard.js
@@ -6,15 +6,15 @@ const image_base = 'https://image.tmdb.org/t/p/original/';
 export const ContentCard = ({ title, name, backdrop_path, poster_path }) => {
   const characterLimit = 30;
   return (
-    <div className='border-2 border-gray-600 hover:bg-gray-600 transition-all'>
+    <div className='hover:bg-gray-600 transition-all'>
       <img
         src={
           poster_path ? image_base + poster_path : image_base + backdrop_path
         }
-        className='xs:p-0 md:pb-2 xl:pb-4 cursor-pointer'
+        className='border-2 border-gray-600 cursor-pointer'
         loading='lazy'
       />
-      <p className='px-1 pb-3 cursor-pointer transition-all hover:scale-[1.2]'>
+      <p className='px-1 py-4 cursor-pointer transition-all hover:scale-[1.2]'>
         {title
           ? title.length > characterLimit
             ? truncate(title, characterLimit)
